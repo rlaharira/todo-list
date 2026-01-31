0cd3999058ad5dcaa3d2a15fd96867b33052b8af
rlaharira <rlahari613@gmail.com>
2026-01-31 21:46:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+unction addTask() {
+  let task = document.getElementById("task").value;
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = task;
+
+  document.getElementById("list").appendChild(li);
+  document.getElementById("task").value = "";
+}
